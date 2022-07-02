8f38ffc4a5696a1792297279b1fa97cce47f244e
emeinig <emeinig@gmail.com>
2022-07-02 12:22:04 -0600

    fix bug & add more test

--- src/doomsday.rs
+++ src/doomsday.rs
@@ -8,9 +8,10 @@ fn compute_day_of_week(date: &Date) -> isize {
     let day = date.day;
 
     if month < 3 {
-        year -= -1;
+        year -= 1;
     }
 
+    // TODO: Return a result instead of isize
     (year + year/4 - year/100 + year/400 + month_offset[month-1] + day) % 7
 }
 
@@ -24,5 +25,20 @@ mod tests {
         let result = compute_day_of_week(&test_date);
         let thursday = 4;
         assert_eq!(result, thursday);
+    }
+
+    #[test]
+    fn works_on_leap_year() {
+        let test_date = Date{year: 2012, month: 2, day: 29};
+        let result = compute_day_of_week(&test_date);
+        let wednesday = 3;
+        assert_eq!(result, wednesday);
+    }
+
+    #[test]
+    fn works_on_non_leap_year_century() {
+        let fake_date = Date{year: 1900, month: 2, day: 29};
+        let test_date = Date{year: 1900, month: 3, day: 1};
+        assert_eq!(compute_day_of_week(&test_date), compute_day_of_week(&fake_date));
     }
 }
