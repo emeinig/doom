ec9b0351f6a560e73fb3b51a5e4183a3e4e4308a
emeinig <emeinig@gmail.com>
2022-07-03 13:02:37 -0600

    improve parser and add error handling

--- src/cli.rs
+++ src/cli.rs
@@ -4,22 +4,35 @@ use clap::Parser;
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
 pub struct Cli {
-    #[clap(value_parser)]
+    #[clap(value_parser = nonempty_string_or_none)]
     date: Option<String>,
 }
 
-pub fn parse_date() -> Option<Date> {
-    let cli = Cli::try_parse().ok()?;
+fn nonempty_string_or_none(s: &str) -> Result<String, &'static str> {
+    if s.trim().is_empty() {
+        Err("Empty Date Field")
+    } else {
+        Ok(s.to_string())
+    }
+}
 
-    if let Some(date_string) = cli.date {
-        let date_vec = date_string.split("-").collect::<Vec<&str>>();
+pub fn parse_date() -> Result<Date, &'static str> {
+    let cli = Cli::parse();
 
-        Some(Date {
-            year: date_vec[2].parse::<isize>().unwrap(),
-            month: date_vec[0].parse::<usize>().unwrap(),
-            day: date_vec[1].parse::<isize>().unwrap(),
+    let date_string = cli.date.ok_or("No Date Given")?;
+
+    let date_vec = date_string.trim().split("-").collect::<Vec<&str>>();
+    let year = date_vec[2].parse::<isize>();
+    let month = date_vec[0].parse::<usize>();
+    let day = date_vec[1].parse::<isize>();
+
+    if year.is_ok() && month.is_ok() && day.is_ok() {
+        Ok(Date {
+            year: year.unwrap(),
+            month: month.unwrap(),
+            day: day.unwrap(),
         })
     } else {
-        None
+        Err("Incorrectly formatted date")
     }
 }
