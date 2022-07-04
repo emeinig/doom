a3c3bc95663ba85a0f916c795c1be4b54e1c54ea
emeinig <emeinig@gmail.com>
2022-07-03 23:04:55 -0600

    add error printing + termination

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 use doom::cli::parse_date;
 use doom::doomsday::compute_day_of_week;
+use std::process;
 
 fn main() {
-    let date = parse_date().unwrap();
+    let date = parse_date().unwrap_or_else(|err| {
+        eprintln!("Error: {}", err);
+        process::exit(1)
+    });
     let day_of_week = compute_day_of_week(&date);
 
     println!(
